10117d2801fc914060f891acb04fd7b8e1dafe79
Eduardo Reche Lopes <eduardo.rechelopes@exxonmobil.com>
2023-03-21 14:56:35 -0300

    style: eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import { app } from './app';
-import { env } from './env';
+import { app } from './app'
+import { env } from './env'
 
-
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT,
-}).then(() => console.log(`🚀🚀🚀 HTTP Server running on port ${env.PORT}!`));
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`🚀🚀🚀 HTTP Server running on port ${env.PORT}!`)
+  })
