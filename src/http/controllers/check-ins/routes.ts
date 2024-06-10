57f4a92922353f9a98aff4340da03acb344d5c55
Eduardo Reche <edu.reche@gmail.com>
2024-06-10 20:17:20 -0300

    test: added check-ins e2e tests

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -9,7 +9,8 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/gyms/:gymId/check-ins', create)
-  app.patch('/check-ins/history', history)
-  app.patch('/check-ins/metrics', metrics)
+
+  app.get('/check-ins/history', history)
+  app.get('/check-ins/metrics', metrics)
   app.patch('/check-ins/:checkInId/validate', validate)
 }
